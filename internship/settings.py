5382087a2c11a0c43e732e5d608260de5acaba02
Musulmankul <musulmankulmamadiev@gmail.com>
2026-06-27 22:20:20 +0600

    First steps

--- internship/settings.py
+++ internship/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3c-iy3575*lys$2z*r9g$dpas600$s*6#!l3j4eqd()8*^+y5=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'api',
+    'wallets',
+    'transactions',
+    'users',
+    
+    'tailwind',
+    'theme'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +57,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    # Add django_browser_reload only in DEBUG mode
+    INSTALLED_APPS += ["django_browser_reload"]
+    MIDDLEWARE += [
+            "django_browser_reload.middleware.BrowserReloadMiddleware",
+        ]
+
 
 ROOT_URLCONF = 'internship.urls'
 
@@ -74,8 +91,12 @@ WSGI_APPLICATION = 'internship.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'interndb',
+        'USER': 'musulmankul',
+        'PASSWORD': 'musulmankul2005',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -115,8 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
