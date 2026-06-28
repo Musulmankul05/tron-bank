c201862c5900503d533abb426f656ea0edbcaaed
Musulmankul <musulmankulmamadiev@gmail.com>
2026-06-28 14:25:36 +0600

    fixes

--- tron/asgi.py
+++ tron/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for internship project.
+ASGI config for tron project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tron.settings')
 
 application = get_asgi_application()
