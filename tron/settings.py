2b9740b10fac9887d82f577b5e6bf2b2e7a280f4
Musulmankul <musulmankulmamadiev@gmail.com>
2026-06-28 14:08:54 +0600

    Created & migrated users model

--- tron/settings.py
+++ tron/settings.py
@@ -9,21 +9,23 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3c-iy3575*lys$2z*r9g$dpas600$s*6#!l3j4eqd()8*^+y5='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
@@ -44,7 +46,8 @@ INSTALLED_APPS = [
     'users',
     
     'tailwind',
-    'theme'
+    'theme',
+    'django_countries'
 ]
 
 
@@ -92,11 +95,11 @@ WSGI_APPLICATION = 'tron.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'interndb',
-        'USER': 'musulmankul',
-        'PASSWORD': 'musulmankul2005',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
 
@@ -139,6 +142,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+AUTH_USER_MODEL = "users.UserModel"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django_countries.fields import CountryField
 
-# Create your models here.
+class UserModel(AbstractUser):
+    STATUS = (
+        ("PEN", "Pending"),
+        ("UNV", "Unverified"),
+        ("VER", "Verified"),
+        ("REJ", "Rejected")
+    )
+    kyc_status = models.CharField(max_length = 3, choices = STATUS, default="UNV", null=False)
+    phone = models.CharField(max_length=32, null=False, blank=False, unique=True)
+    date_birth = models.DateField(blank=False, null=False)
+    country = CountryField(null=False, blank=False)
