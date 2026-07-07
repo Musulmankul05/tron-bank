f8a8cfd9e7036a8a24e78c1ba46acbba9328f533
hikonakura <musulmankulmamadiev@gmail.com>
2026-07-07 23:31:26 +0600

    wallet model

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from users.models import UserModel
+
 # Create your models here.
+class WalletModel(models.Model):
+    owner = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="wallets")
+    currency = models.CharField()
