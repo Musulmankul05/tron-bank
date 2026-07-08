fcfcd65b207d073427a8b8a1dea89be99bc2a3e3
hikonakura <musulmankulmamadiev@gmail.com>
2026-07-08 13:36:36 +0600

    race condition fix

--- transactions/services.py
+++ transactions/services.py
@@ -3,21 +3,28 @@ from transactions.models import TransactionModel
 from decimal import Decimal
 
 def execute_transfer(sender_wallet, receiver_wallet, amount_to_send: Decimal):
+    if amount_to_send <= Decimal('0'):
+            raise ValueError("Amount must be greater than zero")
+            
     with transaction.atomic():
-        after_fee = amount_to_send + (amount_to_send * Decimal('0.01')
-        if sender_wallet.balance < after_fee:
+        sender = sender_wallet.__class__.objects.select_for_update().get(pk=sender_wallet.pk)
+        receiver = receiver_wallet.__class__.objects.select_for_update().get(pk=receiver_wallet.pk)
+        
+        after_fee = amount_to_send + (amount_to_send * Decimal('0.012'))
+        if sender.balance < after_fee:
             raise ValueError("Insufficient balance")
             
-        receiver_wallet.balance += amount_to_send
-        sender_wallet.balance -= after_fee
-        sender_wallet.save()
-        receiver_wallet.save()
+        receiver.balance += amount_to_send
+        sender.balance -= after_fee
+        
+        sender.save(update_fields=['balance'])
+        receiver.save(update_fields=['balance'])
         TransactionModel.objects.create(
-            sender=sender_wallet,
-            receiver=receiver_wallet,
+            sender=sender,
+            receiver=receiver,
             sent=after_fee,
             received=amount_to_send,
-            fee=amount_to_send * Decimal('0.01'),
+            fee=amount_to_send * Decimal('0.012'),
             status='OK',
         )
         return True
